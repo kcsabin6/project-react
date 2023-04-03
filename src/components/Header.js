5f3122dacca1d769bfd7c83a1830fb4bf06af269
sabin <kcsabin6@gmail.com>
2023-04-02 22:49:59 -0500

    user form add, delete user

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react"; 
+
+const Header = () => {
+    return (
+        <div className="ui one column stackable center aligned page grid">
+        <div className="column twelve wide">
+            <h2>Contact Manager</h2>
+        </div>
+     </div>
+    );
+}
+
+export default Header;
